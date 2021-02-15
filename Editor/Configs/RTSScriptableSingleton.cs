e22c6e6ade5282c92aab81de7c1507b0155f9341
vyronlee <lwz_jz@hotmail.com>
2021-02-15 23:10:19 +0800

    Add frameworks.

---
+++ Editor/Configs/RTSScriptableSingleton.cs
@@ -0,0 +1,69 @@
+using System;
+using UnityEditorInternal;
+using UnityEngine;
+
+namespace vFrame.ResourceToolset.Editor.Configs
+{
+    internal class RTSScriptableSingleton<T> : ScriptableObject where T : ScriptableObject
+    {
+        private static T _instance;
+
+        protected RTSScriptableSingleton() {
+            if (_instance != null)
+                Debug.LogError("ScriptableSingleton already exists. Did you query the singleton in a constructor?");
+            else
+                _instance = (object) this as T;
+        }
+
+        public static T Instance {
+            get {
+                if (_instance == null)
+                    CreateAndLoad();
+                return _instance;
+            }
+        }
+
+        private static void CreateAndLoad() {
+            var filePath = GetFilePath();
+            if (!string.IsNullOrEmpty(filePath))
+                InternalEditorUtility.LoadSerializedFileAndForget(filePath);
+            if (_instance != null)
+                return;
+            CreateInstance<T>().hideFlags = HideFlags.HideAndDontSave;
+        }
+
+        private static string GetFilePath() {
+            foreach (var customAttribute in typeof(T).GetCustomAttributes(true))
+                if (customAttribute is FilePathAttribute attribute)
+                    return attribute.Filepath;
+
+            return null;
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    internal class FilePathAttribute : Attribute
+    {
+        public enum Location
+        {
+            PreferencesFolder,
+            ProjectFolder
+        }
+
+        public FilePathAttribute(string relativePath, Location location) {
+            if (string.IsNullOrEmpty(relativePath)) {
+                Debug.LogError("Invalid relative path! (its null or empty)");
+            }
+            else {
+                if (relativePath[0] == '/')
+                    relativePath = relativePath.Substring(1);
+                if (location == Location.PreferencesFolder)
+                    Filepath = InternalEditorUtility.unityPreferencesFolder + "/" + relativePath;
+                else
+                    Filepath = relativePath;
+            }
+        }
+
+        public string Filepath { get; set; }
+    }
+}
---
+++ Editor/Exceptions/ResourceToolsetException.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace vFrame.ResourceToolset.Editor.Exceptions
+{
+    public class ResourceToolsetException : Exception
+    {
+        public ResourceToolsetException() {
+
+        }
+
+        public ResourceToolsetException(string message) : base(message) {
+
+        }
+    }
+}
