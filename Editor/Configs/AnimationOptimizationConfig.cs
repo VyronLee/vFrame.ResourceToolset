dcc89d61e1ca6ce8b4cd3cbeb7454e8f60d0721b
VyronLee <lwz_jz@hotmail.com>
2021-02-22 20:00:16 +0800

    Add animation optimization utils.

---
+++ Editor/Configs/AnimationOptimizationConfig.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using vFrame.ResourceToolset.Editor.Const;
+
+namespace vFrame.ResourceToolset.Editor.Configs
+{
+    [CreateAssetMenu(
+        fileName = nameof(AnimationOptimizationConfig),
+        menuName = ToolsetConst.AnimationOptimizationConfigMenuName,
+        order = 0)]
+    [FilePath(
+        ToolsetConst.AnimationOptimizationConfigFilePath,
+        FilePathAttribute.Location.ProjectFolder)]
+    public class AnimationOptimizationConfig : ScriptableSingleton<AnimationOptimizationConfig>
+    {
+        [SerializeField]
+        [Range(1, 6)]
+        private uint _precision = 3;
+
+        public uint Precision => _precision;
+    }
+}
---
+++ Editor/Menus/AnimationCurveOptimizer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+using vFrame.ResourceToolset.Editor.Configs;
+using vFrame.ResourceToolset.Editor.Const;
+using vFrame.ResourceToolset.Editor.Utils;
+
+namespace vFrame.ResourceToolset.Editor.Menus
+{
+    internal static class AnimationCurveOptimizer
+    {
+        private static readonly string[] ManagedAssetExtensions = {".anim"};
+
+        [MenuItem(ToolsetConst.AssetsMenuDir + "Animation/Modify Animation Curve Precision")]
+        private static void ModifyAnimationCurvePrecision() {
+            var precision = AnimationOptimizationConfig.Instance.Precision;
+
+            bool Optimize(Object obj) {
+                return obj is AnimationClip clip
+                       && AnimationOptimizationUtils.ModifyCurveValuesPrecision(clip, precision);
+            }
+
+            AssetProcessorUtils.TravelAndProcessSelectedObjects(ManagedAssetExtensions,
+                "Modify Animation Curve Precision",
+                Optimize);
+        }
+
+        [MenuItem(ToolsetConst.AssetsMenuDir + "Animation/Remove Animation Scale Curve")]
+        private static void RemoveAnimationScaleCurve() {
+            bool Optimize(Object obj) {
+                return obj is AnimationClip clip
+                       && AnimationOptimizationUtils.RemoveScaleCurve(clip);
+            }
+
+            AssetProcessorUtils.TravelAndProcessSelectedObjects(ManagedAssetExtensions,
+                "Remove Animation Scale Curve",
+                Optimize);
+        }
+    }
+}
