e24a5a9ce1b212005784fe8e1ee4718df6df1d93
VyronLee <lwz_jz@hotmail.com>
2021-03-01 07:19:28 +0800

    Add missing reference finder utils.

---
+++ Assets/vFrame.ResourceToolset/Editor/Menus/MissingReferenceFinder.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using vFrame.ResourceToolset.Editor.Const;
+using vFrame.ResourceToolset.Editor.Utils;
+
+namespace vFrame.ResourceToolset.Editor.Menus
+{
+    internal static class MissingReferenceFinder
+    {
+        private static readonly string[] ManagedAssetExtensions = {
+            ".unity",
+            ".prefab",
+            ".mat",
+            ".anim",
+            ".controller",
+            ".asset"
+        };
+
+        [MenuItem(ToolsetConst.AssetsMenuDir + "/Find Missing Reference")]
+        private static void FindMissingReference() {
+            var missing = new List<string>();
+            void Validate(Object obj) {
+                if (MissingReferenceValidationUtils.ValidateAsset(obj, out var missingObjects))
+                    return;
+
+                var info = AssetDatabase.GetAssetPath(obj);
+                info += "\n";
+                info = missingObjects.Aggregate(info,
+                    (current, missingObject) => current + "\t" + missingObject + "\n");
+
+                missing.Add(info);
+            }
+
+            AssetProcessorUtils.TravelSelectedObjects(ManagedAssetExtensions,
+                "Find Missing Reference",
+                Validate);
+
+            if (missing.Count <= 0) {
+                Debug.Log("Find missing reference finished, nothing wrong detected.");
+                return;
+            }
+
+            Debug.Log("Find missing reference finished, reference list below are missing: \n"
+                      + string.Join("\n", missing.ToArray()));
+        }
+    }
+}
