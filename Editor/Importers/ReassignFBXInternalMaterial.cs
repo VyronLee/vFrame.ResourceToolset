359389176a9258bcc23f1978fcb35cf051107d99
VyronLee <lwz_jz@hotmail.com>
2021-03-17 20:49:35 +0800

    Add model importer to reassign fbx internal materials.

---
+++ Editor/Importers/ReassignFBXInternalMaterial.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+using vFrame.ResourceToolset.Editor.Configs;
+
+namespace vFrame.ResourceToolset.Editor.Importers
+{
+    public class ModelImporter : AssetPostprocessor
+    {
+        protected Material OnAssignMaterialModel(Material previousMaterial, Renderer renderer) {
+            if (!BuiltinAssetConfig.Instance.AutoReplaceFBXInternalMaterialOnImport) {
+                return previousMaterial;
+            }
+            var material = BuiltinAssetConfig.Instance.FBXInternalMaterialReplacement;
+            if (material) {
+                return material;
+            }
+            Debug.LogError("FBX internal material must be specified.");
+            return previousMaterial;
+        }
+    }
+}
