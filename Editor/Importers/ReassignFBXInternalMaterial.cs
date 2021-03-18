c78b4ab30da476458b2ce8fddc3c3ed6698e35c7
VyronLee <lwz_jz@hotmail.com>
2021-03-18 12:04:28 +0800

    Fix reassign fbx material problem.

--- Editor/Importers/ReassignFBXInternalMaterial.cs
+++ Editor/Importers/ReassignFBXInternalMaterial.cs
@@ -1,11 +1,18 @@
-﻿using UnityEditor;
+﻿using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using vFrame.ResourceToolset.Editor.Configs;
 
 namespace vFrame.ResourceToolset.Editor.Importers
 {
-    public class ModelImporter : AssetPostprocessor
+    public class ReassignFBXInternalMaterial : AssetPostprocessor
     {
+        /// <summary>
+        /// Reassign SkinnedMeshRenderer material.
+        /// </summary>
+        /// <param name="previousMaterial"></param>
+        /// <param name="renderer"></param>
+        /// <returns></returns>
         protected Material OnAssignMaterialModel(Material previousMaterial, Renderer renderer) {
             if (!BuiltinAssetConfig.Instance.AutoReplaceFBXInternalMaterialOnImport) {
                 return previousMaterial;
@@ -16,6 +23,28 @@ namespace vFrame.ResourceToolset.Editor.Importers
             }
             Debug.LogError("FBX internal material must be specified.");
             return previousMaterial;
+        }
+
+        /// <summary>
+        /// Reassign MeshRenderer material.
+        /// </summary>
+        /// <param name="go"></param>
+        private void OnPostprocessModel(GameObject go) {
+            if(!BuiltinAssetConfig.Instance.AutoReplaceFBXInternalMaterialOnImport) {
+                return;
+            }
+
+            var material = BuiltinAssetConfig.Instance.FBXInternalMaterialReplacement;
+            if (!material) {
+                Debug.LogError("FBX internal material must be specified.");
+                return;
+            }
+
+            var meshRenderers = go.GetComponentsInChildren<MeshRenderer>(true);
+            foreach (var meshRenderer in meshRenderers) {
+                meshRenderer.sharedMaterials =
+                    Enumerable.Repeat(material, meshRenderer.sharedMaterials.Length).ToArray();
+            }
         }
     }
 }
