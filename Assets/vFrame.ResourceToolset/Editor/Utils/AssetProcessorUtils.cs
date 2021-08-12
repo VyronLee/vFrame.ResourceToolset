a313b98b04c8265464131106946ed76ab06732af
VyronLee <lwz_jz@hotmail.com>
2021-08-12 20:19:00 +0800

    Optimize assets processors memory usage.

--- Assets/vFrame.ResourceToolset/Editor/Utils/AssetProcessorUtils.cs
+++ Assets/vFrame.ResourceToolset/Editor/Utils/AssetProcessorUtils.cs
@@ -10,22 +10,24 @@ namespace vFrame.ResourceToolset.Editor.Utils
 {
     public static class AssetProcessorUtils
     {
-        public static List<Object> GetSelectedObjects(string[] extensions) {
+        private const int UnloadAssetsOnProcessedCount = 200;
+
+        public static List<string> GetSelectedObjects(string[] extensions) {
             var selection = Selection.activeObject;
             var path = AssetDatabase.GetAssetPath(selection);
             if (!AssetDatabase.IsValidFolder(path))
-                return new List<Object> {selection};
+                return new List<string> {AssetDatabase.AssetPathToGUID(path)};
 
             return GetObjectsInDirectory(path, extensions);
         }
 
-        public static List<Object> GetObjectsInDirectory(string dir, string[] extensions) {
+        public static List<string> GetObjectsInDirectory(string dir, string[] extensions) {
             if (string.IsNullOrEmpty(dir)) {
                 throw new ArgumentException("Argument cannot be null", nameof(dir));
             }
 
             var assets = AssetDatabase.FindAssets("t:Object", new[] {dir});
-            var objects = new List<Object>();
+            var objects = new List<string>();
             var index = 0f;
             foreach (var asset in assets) {
                 var p = AssetDatabase.GUIDToAssetPath(asset);
@@ -35,12 +37,7 @@ namespace vFrame.ResourceToolset.Editor.Utils
                     continue;
                 }
 
-                var obj = AssetDatabase.LoadAssetAtPath<Object>(p);
-                if (!obj) {
-                    Debug.LogError("Load asset at path failed: " + p);
-                    continue;
-                }
-                objects.Add(obj);
+                objects.Add(asset);
             }
 
             EditorUtility.ClearProgressBar();
@@ -68,11 +65,18 @@ namespace vFrame.ResourceToolset.Editor.Utils
             TravelObjects(objects.ToArray(), title, processor);
         }
 
-        private static void TravelAndProcessObjects(IReadOnlyCollection<Object> objects, string title, Func<Object, bool> processor) {
+        private static void TravelAndProcessObjects(IReadOnlyCollection<string> objects, string title, Func<Object, bool> processor) {
             var index = 0f;
             var changed = new List<string>();
             try {
-                foreach (var obj in objects) {
+                foreach (var objGuid in objects) {
+                    if (index % UnloadAssetsOnProcessedCount == 0) {
+                        Resources.UnloadUnusedAssets();
+                        GC.Collect();
+                    }
+
+                    var objPath = AssetDatabase.GUIDToAssetPath(objGuid);
+                    var obj = AssetDatabase.LoadAssetAtPath<Object>(objPath);
                     if (!obj) {
                         continue;
                     }
@@ -107,16 +111,22 @@ namespace vFrame.ResourceToolset.Editor.Utils
                 + string.Join("\n", changed.ToArray()));
         }
 
-        private static void TravelObjects(IReadOnlyCollection<Object> objects, string title, Action<Object> processor) {
+        private static void TravelObjects(IReadOnlyCollection<string> objects, string title, Action<Object> processor) {
             var index = 0f;
             try {
-                foreach (var obj in objects) {
+                foreach (var objGuid in objects) {
+                    if (index % UnloadAssetsOnProcessedCount == 0) {
+                        Resources.UnloadUnusedAssets();
+                        GC.Collect();
+                    }
+
+                    var objPath = AssetDatabase.GUIDToAssetPath(objGuid);
+                    var obj = AssetDatabase.LoadAssetAtPath<Object>(objPath);
                     if (!obj) {
                         continue;
                     }
 
-                    var path = AssetDatabase.GetAssetPath(obj);
-                    if (EditorUtility.DisplayCancelableProgressBar(title, path, ++index / objects.Count)) {
+                    if (EditorUtility.DisplayCancelableProgressBar(title, objPath, ++index / objects.Count)) {
                         break;
                     }
 
